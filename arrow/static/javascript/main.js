66ba2985263e16bc8b51124bbd5eeb0498381c54
Dhruvil-Gajjar <dhruvilvi.dev@gmail.com>
2020-12-07 18:04:38 +0530

    home

--- arrow/static/javascript/main.js
+++ arrow/static/javascript/main.js
@@ -58,3 +58,12 @@ function deleteUser(id) {
         });
     }
 }
+
+
+// Intro carousel
+  var heroCarousel = $("#heroCarousel");
+
+  heroCarousel.on('slid.bs.carousel', function(e) {
+    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+    $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
+  });
